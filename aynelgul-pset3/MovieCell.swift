d3cf2a572c3ca00c18ef2621886dbc69ccc5c5cc
Aynel Gül <aynell__@hotmail.com>
2016-11-17 12:04:58 +0100

    opzet

---
+++ aynelgul-pset3/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  aynelgul-pset3
+//
+//  Created by Aynel Gül on 16-11-16.
+//  Copyright © 2016 Aynel Gül. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieDiscription: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- aynelgul-pset3/ViewController.swift
+++ aynelgul-pset3/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
 
