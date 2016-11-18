4ffe8f6979bb068b2f66c6aa950965f2906b395a
Aynel Gül <aynell__@hotmail.com>
2016-11-18 15:04:30 +0100

    data wordt opgehaald en in joon gezet

--- aynelgul-pset3/ViewController.swift
+++ aynelgul-pset3/ViewController.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    @IBOutlet weak var TableView: UITableView!
+    @IBOutlet weak var movieRequest: UITextField!
+    @IBAction func findMovie(_ sender: UITextField) {
+        HTTPSrequest(title: movieRequest.text!)
+    }
+    
+    var titles = [String]()
+    var descriptions = [String]()
+    
+    
+    func HTTPSrequest(title: String) {
+        let title = movieRequest.text!.replacingOccurrences(of: " ", with: "+")
+
+        let url = URL(string: "https://omdbapi.com/?t="+title+"&yplot=short&r=json")
+        
+        let task = URLSession.shared.dataTask(with: url!, completionHandler: { data, response, error in
+            guard let data = data, error == nil else {
+                // error?
+                return
+            }
+            do {
+                
+                let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                print (json)
+                
+                DispatchQueue.main.async {
+                    self.titles.append(json["Title"] as! String)
+                    self.descriptions.append(json["Plot"] as! String)
+                }
+            } catch {
+                // error handling
+            }
+
+        }).resume()
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,11 +61,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MovieCell
+        
+        cell.movieDiscription.text = titles[indexPath.row]
+        cell.movieDiscription.text = descriptions[indexPath.row]
+        
+        return cell
     }
     
 }
